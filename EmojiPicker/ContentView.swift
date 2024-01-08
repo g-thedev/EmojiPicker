82663023478daae24f03dae8991009351d8ad3d7
Gerard Fajardo <gerard@zeta.haus>
2024-01-08 14:10:59 -0800

    Initial Commit

---
+++ EmojiPicker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EmojiPicker
+//
+//  Created by Gerard Fajardo on 2024-01-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EmojiPicker/EmojiPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPickerApp.swift
+//  EmojiPicker
+//
+//  Created by Gerard Fajardo on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
